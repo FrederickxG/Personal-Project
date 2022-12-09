8a7593cb88ef0a1f9a0f7563d99183361385b2ad
FrederickxG <fgardnerjr24@aoiths.org>
2022-12-09 12:46:17 -0500

    Lab 3 V1

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+   
+    public GameObject KunaiPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send Kunai
+        if (Input.GetKeyDown(KeyCode.Space))
+    {
+         Instantiate(KunaiPrefab, transform.position, KunaiPrefab.transform.rotation);
+    }
+       
+     }
+
+   
+}
